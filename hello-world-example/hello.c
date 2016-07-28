cfe1c963dffae6001f55725847e1ec8e5536d491
katzhang <arcadychang@gmail.com>
2016-07-27 21:48:20 -0400

    added hello world example

---
+++ hello-world-example/hello.c
@@ -0,0 +1,17 @@
+#include <linux/module.h>    // included for all kernel modules
+#include <linux/kernel.h>    // included for KERN_INFO
+#include <linux/init.h>      // included for __init and __exit macros
+
+static int __init hello_init(void)
+{
+    printk(KERN_INFO "Hello, world\n");
+    return 0;    // Non-zero return means that the module couldn't be loaded.
+}
+
+static void __exit hello_cleanup(void)
+{
+  printk(KERN_INFO "Good-bye.\n");
+}
+
+module_init(hello_init);
+module_exit(hello_cleanup);
